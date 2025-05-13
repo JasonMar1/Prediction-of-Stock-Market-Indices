dc6ca6187c4bf87ba86e02b0a82767d1af0bacf6
iason <iasonasmarinopoulos@gmail.com>
2025-05-14 02:56:12 +0300

    - DeepAR, TFT monthly data_loaders implemented - DeepAR, TFT monthly optuna & optuna scheduler implemented & ready to be executed - DeepAR, TFT monthly scheduler optuna implemented & ready to be executed

--- data_loader_DeepAR.py
+++ data_loader_DeepAR.py
@@ -101,3 +101,78 @@ def return_splits(TRAIN_START_DATE, TRAIN_END_DATE, VALID_START_DATE, VALID_END_
     print(f"Feature columns: {feature_columns}")
 
     return df_train, df_val, df_test, feature_columns
+
+
+def load_index_monthly_data(index_name, TRAIN_START_DATE, TEST_END_DATE):
+    file_path = indices_conditional[index_name]
+    df = pd.read_csv(file_path, parse_dates=["Date"], index_col="Date")
+    df = df[df.index >= "1950-01-03"]
+    df = df.loc[TRAIN_START_DATE:TEST_END_DATE]
+
+    monthly_df = df.resample("ME").agg({
+        "Open": "first",
+        "High": "max",
+        "Low": "min",
+        "Close": "last",
+        "Adjusted_close": "last",
+        "Volume": "sum"
+    })
+
+    monthly_df["Log_Returns"] = np.log(monthly_df["Adjusted_close"]) - np.log(monthly_df["Adjusted_close"].shift(1))
+    monthly_df["target"] = monthly_df["Log_Returns"].shift(-1)
+
+    monthly_df["Log_Returns_2"] = np.log(monthly_df["Adjusted_close"]) - np.log(monthly_df["Adjusted_close"].shift(2))
+    monthly_df["Log_Returns_4"] = np.log(monthly_df["Adjusted_close"]) - np.log(monthly_df["Adjusted_close"].shift(4))
+    monthly_df["Log_Returns_6"] = np.log(monthly_df["Adjusted_close"]) - np.log(monthly_df["Adjusted_close"].shift(6))
+
+    monthly_df["Volatility"] = monthly_df["Log_Returns"].rolling(window=3).std()
+    monthly_df["RSI_6"] = compute_RSI(monthly_df, period=6)
+
+    monthly_df["SMA_2"] = monthly_df["Adjusted_close"].rolling(window=2).mean()
+    monthly_df["SMA_6"] = monthly_df["Adjusted_close"].rolling(window=6).mean()
+    monthly_df["SMA_12"] = monthly_df["Adjusted_close"].rolling(window=12).mean()
+
+    monthly_df["EMA_2"] = monthly_df["Adjusted_close"].ewm(span=2, adjust=False).mean()
+    monthly_df["EMA_6"] = monthly_df["Adjusted_close"].ewm(span=6, adjust=False).mean()
+
+    monthly_df["MA_Crossover"] = monthly_df["SMA_2"] > monthly_df["SMA_6"]
+
+    # Extra metadata
+    monthly_df["index_id"] = index_name
+    monthly_df["date"] = monthly_df.index
+    monthly_df["month"] = monthly_df.index.month.astype(str)
+
+    monthly_df.dropna(inplace=True)
+    return monthly_df
+
+def load_all_indices_monthly(TRAIN_START_DATE, TEST_END_DATE):
+    all_dfs = []
+    for index_name in selected_indices.values():
+        print(f"Loading monthly data for {index_name}...")
+        df = load_index_monthly_data(index_name, TRAIN_START_DATE, TEST_END_DATE)
+        all_dfs.append(df)
+
+    df_all = pd.concat(all_dfs).sort_values(["index_id", "date"]).reset_index(drop=True)
+
+    # Add time_idx (per series)
+    df_all["time_idx"] = df_all.groupby("index_id").cumcount()
+    return df_all
+
+def return_splits_monthly(TRAIN_START_DATE, TRAIN_END_DATE, VALID_START_DATE, VALID_END_DATE, TEST_START_DATE, TEST_END_DATE):
+    df_all = load_all_indices_monthly(TRAIN_START_DATE, TEST_END_DATE)
+
+    df_all["date"] = pd.to_datetime(df_all["date"])
+
+    df_train = df_all[(df_all["date"] >= pd.to_datetime(TRAIN_START_DATE)) & (df_all["date"] <= pd.to_datetime(TRAIN_END_DATE))]
+    df_val = df_all[(df_all["date"] >= pd.to_datetime(VALID_START_DATE)) & (df_all["date"] <= pd.to_datetime(VALID_END_DATE))]
+    df_test = df_all[(df_all["date"] >= pd.to_datetime(TEST_START_DATE)) & (df_all["date"] <= pd.to_datetime(TEST_END_DATE))]
+
+    # Columns to be used in time_varying_known_reals
+    excluded_columns = ["Adjusted_close", "target", "date", "index_id", "time_idx", "month"]
+    feature_columns = [col for col in df_all.columns if col not in excluded_columns]
+
+    print("Monthly DeepAR splits created:")
+    print(f"Train: {df_train.shape}, Val: {df_val.shape}, Test: {df_test.shape}")
+    print(f"Feature columns: {feature_columns}")
+
+    return df_train, df_val, df_test, feature_columns
---
+++ monthly models/Monthly Optuna/DeepAR monthly optuna.py
@@ -0,0 +1,106 @@
+from data_loader_DeepAR import return_splits_monthly
+import torch
+import optuna
+
+from pytorch_forecasting import TimeSeriesDataSet, DeepAR, GroupNormalizer
+from pytorch_forecasting.metrics import NormalDistributionLoss
+from lightning.pytorch import Trainer, seed_everything
+from lightning.pytorch.callbacks import EarlyStopping
+
+
+def objective(trial):
+    print("-"*50)
+    print(f"\n Trial {trial.number}")
+
+    # Define hyperparameter search space
+    hidden_size = trial.suggest_int("hidden_size", 32, 512, log=True)
+    rnn_layers = trial.suggest_int("rnn_layers", 1, 4)
+    dropout = trial.suggest_float("dropout", 0.0, 0.5, step=0.05)
+    learning_rate = trial.suggest_float("learning_rate", 1e-4, 1e-2, log=True)
+    batch_size = trial.suggest_int("batch_size", 16, 128, step=16)
+    max_encoder_length = trial.suggest_int("max_encoder_length", 1, 3)
+    epochs = trial.suggest_int("epochs", 10, 100, step=5)
+    # epochs = 100
+
+    train_dataset = TimeSeriesDataSet(
+        df_train,
+        time_idx="time_idx",
+        target="target",
+        group_ids=["index_id"],  # time step of each observation (sequential per group)
+        max_encoder_length=max_encoder_length,
+        max_prediction_length=prediction_length,
+        static_categoricals=["index_id"],  # Categorical variables that don't change over time
+        time_varying_known_categoricals=["month"],  # Categorical variables known in advance and change over time
+        time_varying_known_reals=feature_columns,  # Real-valued features that are known at prediction time
+        time_varying_unknown_reals=["target"],   # Real-valued features that are not known in advance and must be predicted
+        target_normalizer=GroupNormalizer(groups=["index_id"]),  # z-score
+        allow_missing_timesteps=True,
+    )
+
+    val_dataset = TimeSeriesDataSet.from_dataset(
+        train_dataset, df_val, stop_randomization=True
+    )
+
+    train_loader = train_dataset.to_dataloader(train=True, batch_size=batch_size, num_workers=6, persistent_workers=True)
+    val_loader = val_dataset.to_dataloader(train=False, batch_size=batch_size, num_workers=6, persistent_workers=True)
+
+    model = DeepAR.from_dataset(
+        train_dataset,
+        learning_rate=learning_rate,
+        hidden_size=hidden_size,
+        rnn_layers=rnn_layers,
+        dropout=dropout,
+        loss=NormalDistributionLoss(),
+        log_interval=-1,
+        log_val_interval=-1
+    )
+
+    early_stop = EarlyStopping(monitor="val_loss", patience=20)
+    trainer = Trainer(
+        max_epochs=epochs,
+        accelerator="gpu" if torch.cuda.is_available() else "cpu",
+        devices=1,
+        callbacks=[early_stop],
+        gradient_clip_val=0.1,  # limits how big the gradients can get to keep training stable
+        enable_progress_bar=False,
+        num_sanity_val_steps=0  # <- disables the pre-training sanity check
+    )
+
+    trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+    val_loss = trainer.callback_metrics["val_loss"].item()
+
+    print(f"Trial {trial.number} finished with val_loss: {val_loss:.5f}")
+    return val_loss
+
+
+# Callback to log current best trial
+def log_best_trial(study, trial):
+    print(f"Best trial so far: {study.best_trial.number} with val_loss: {study.best_value:.5f}")
+
+if __name__ == "__main__":
+
+    seed_everything(42, workers=True)
+
+    # Static date splits
+    TRAIN_START_DATE = "2006-01-01"
+    TRAIN_END_DATE = "2019-11-30"
+
+    VALID_START_DATE = "2019-12-01"
+    VALID_END_DATE = "2022-08-31"
+
+    TEST_START_DATE = "2022-10-01"  # worst case scenario, having sequence length equal to 3 months + dropping 1 month for data-leakage
+    TEST_END_DATE = "2025-01-01"
+
+    df_train, df_val, df_test, feature_columns = return_splits_monthly(
+        TRAIN_START_DATE, TRAIN_END_DATE,
+        VALID_START_DATE, VALID_END_DATE,
+        TEST_START_DATE, TEST_END_DATE
+    )
+
+    prediction_length = 1  # 1-month ahead
+
+    study = optuna.create_study(direction="minimize")
+    study.optimize(objective, n_trials=500, callbacks=[log_best_trial])
+
+    print("Best hyperparameters:")
+    print(study.best_trial)
